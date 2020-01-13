69f9253365e1fc6571e2bb2fec8c8ddfd793e8e9
Yaroslav Voronin <voronin06.08@yandex.ru>
2020-01-13 17:07:53 +0200

    done

--- gulpfile.js
+++ gulpfile.js
@@ -47,9 +47,9 @@ gulp.task('styles', function () {
 gulp.task('scripts', function () {
 	return gulp.src([
 		// 'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
-		'app/js/_libs.js', // JS libraries (all in one)
+		'app/js/libs.js', // JS libraries (all in one)
 		//'app/js/_copy.js', // JS libraries (all in one)
-		'app/js/_custom.js', // Custom scripts. Always at the end
+		'app/js/custom.js', // Custom scripts. Always at the end
 	])
 		.pipe(concat('scripts.min.js'))
 		.pipe(uglify()) // Minify js (opt.)
